3ccdbc6490e3e78a58069267d47f2a338eaa002b
Mark P <mj.poso18@gmail.com>
2022-11-15 03:19:00 +0800

    Add PWA functionalities

--- src/view/script.js
+++ src/view/script.js
@@ -2,6 +2,10 @@ import '../model/todo.js';
 import './ui/TodoForm.js';
 import './ui/TodoList.js';
 import '../controller/pubsub.js';
-import { Events } from '../controller/pubsub.js';
+
+/* PWA Service Worker */
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js');
+}
 
 document.getElementById('notice').remove();
