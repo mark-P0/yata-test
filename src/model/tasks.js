d631e44a6fe546e58ea1b6683e8c842262ece40f
Mark P <mj.poso18@gmail.com>
2022-11-25 18:35:42 +0800

    Record creation dates on `Task` items

--- src/model/tasks.js
+++ src/model/tasks.js
@@ -2,15 +2,20 @@ import { TaskDate } from './task-dates.js';
 import { generateId } from './id-generator.js';
 
 class Task {
-  static #typeIsId = false;
+  static #toConstructInternally = false;
 
   id;
+  #creationDate;
   title;
   description;
   #dueDate;
   priority;
   constructor(type, title, description, dueDate, priority) {
-    this.id = Task.#typeIsId ? type : generateId(type);
+    if (!Task.#toConstructInternally) {
+      this.id = generateId(type);
+      this.#creationDate = TaskDate.current;
+    }
+
     this.title = title;
     this.description = description;
     this.#dueDate = new TaskDate(dueDate);
@@ -19,18 +24,26 @@ class Task {
   get dueDate() {
     return `${this.#dueDate}`;
   }
+  get creationDate() {
+    return `${this.#creationDate}`;
+  }
 
   static serialize(instance) {
-    const { id, title, description, dueDate, priority } = instance;
-    const object = { title, description, dueDate, priority };
+    const { id, title, description, dueDate, priority, creationDate } =
+      instance;
+    const object = { title, description, dueDate, priority, creationDate };
     const string = JSON.stringify(object);
     return [id, string];
   }
   static deserialize(id, string) {
     const object = JSON.parse(string);
-    this.#typeIsId = true;
-    const instance = new this(id, ...Object.values(object));
-    this.#typeIsId = false;
+
+    this.#toConstructInternally = true;
+    const instance = new this(null, ...Object.values(object));
+    instance.id = id;
+    instance.#creationDate = new TaskDate(object.creationDate);
+    this.#toConstructInternally = false;
+
     return instance;
   }
 }
