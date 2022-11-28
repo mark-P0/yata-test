0eb3958a8c36563cd5737d475ec6936013b78ef6
Mark P <mj.poso18@gmail.com>
2022-11-28 21:39:12 +0800

    Simplify sorting chip definitions

--- src/view/ui/ListSortingChips.js
+++ src/view/ui/ListSortingChips.js
@@ -2,20 +2,11 @@ import { InstanceIDs, SorterIDs } from 'src/model/ids.js';
 import { E } from '../__dom__.js';
 import { Events } from 'src/controller/pubsub.js';
 
-const mapSorterData = {
-  [SorterIDs.CREATION_DATE]: { text: 'Creation', icon: 'pencil-square' },
-  [SorterIDs.PRIORITY]: { text: 'Priority', icon: 'exclamation-square' },
-  [SorterIDs.PROJECT]: { text: 'Project', icon: 'kanban' },
-  [SorterIDs.DUE_DATE]: { text: 'Due Date', icon: 'calendar-event' },
-};
-
-const SortingChipRadio = (group, sorterId) => {
-  const { text, icon } = mapSorterData[sorterId];
-
+const SortingChipRadio = (group, value, text, icon, isDefault = false) => {
   let attributes;
 
   const id = InstanceIDs.generate('HTML');
-  attributes = { class: 'btn-check', type: 'radio', name: group, id };
+  attributes = { class: 'btn-check', type: 'radio', name: group, id, value };
   const input = E('input', attributes);
 
   attributes = {
@@ -32,8 +23,13 @@ const SortingChipRadio = (group, sorterId) => {
   const label = E('label', attributes, text, [background]);
 
   input.addEventListener('click', () => {
-    Events.UPDATE_DISPLAY_SORTING.publish(sorterId);
+    Events.UPDATE_DISPLAY_SORTING.publish(value);
   });
+  if (isDefault) {
+    Events.INITIALIZATION.subscribe(() => {
+      input.click();
+    });
+  }
 
   return [input, label];
 };
@@ -47,17 +43,13 @@ const ListSortingChips = () => {
       'w-100 hstack gap-3 ps-3 pe-5 pt-3 py-2 overflow-auto scrollbar scrollbar-thin scrollbar-translucent',
     id,
   };
-  /** @type {HTMLElement} */
+  /* prettier-ignore */
   const chips = E('div', attributes, [
-    ...SortingChipRadio(id, SorterIDs.CREATION_DATE),
-    ...SortingChipRadio(id, SorterIDs.PRIORITY),
-    // ...SortingChipRadio(id, SorterIDs.PROJECT),
-    ...SortingChipRadio(id, SorterIDs.DUE_DATE),
+    ...SortingChipRadio(id, SorterIDs.CREATION_DATE, 'Creation', 'pencil-square', true),
+    ...SortingChipRadio(id, SorterIDs.PRIORITY, 'Priority', 'exclamation-square'),
+    // ...SortingChipRadio(id, SorterIDs.PROJECT, 'Project', 'kanban'),
+    ...SortingChipRadio(id, SorterIDs.DUE_DATE, 'Due Date', 'calendar-event'),
   ]);
-
-  Events.INITIALIZATION.subscribe(() => {
-    chips.firstElementChild.click(); // Default radio group to first button
-  });
 
   attributes = { class: 'position-absolute top-0 start-0' };
   const container = E('div', attributes, [
